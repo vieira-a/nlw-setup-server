dc5ccf97eb3a3db87524255266166c21495e6eb7
Anderson Vieira <asvieira.br@gmail.com>
2023-01-17 11:27:07 -0300

    feat: setup cors with fastify

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors"
 
 const app = fastify()
-
 const prisma = new PrismaClient()
+app.register(cors)
 
 app.get("/habits", async ()=>{
     const habits = await prisma.habit.findMany()
