783d8cf923bd4f4a1b342a37ba80e374a3c7d3f9
Anderson Vieira <asvieira.br@gmail.com>
2023-01-17 11:23:36 -0300

    feat:  setup prisma and create table habits

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client"
 
 const app = fastify()
 
-app.get("/", ()=>{
-    return "Hello NLW"
+const prisma = new PrismaClient()
+
+app.get("/habits", async ()=>{
+    const habits = await prisma.habit.findMany()
+    return habits
 })
 
 app.listen({
     port: 3333,
+}).then(() => {
+    console.log("HTTP server running...")
 })
